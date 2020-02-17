786bb25606178e83d56996b7ec4476b3e0039670
chahiinaz <chahiinaz@gmail.com>
2020-02-17 17:20:02 +0100

    Create reducer, action

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import SignupFormContainer from "./components/SignUp/SignupFormContainer";
+import LoginContainer from "./components/Login/LoginContainer";
 
 import { Provider } from "react-redux";
 import { Route } from "react-router-dom";
@@ -13,6 +14,7 @@ function App() {
         <div className="heading-primary">
           <h1>Rock, Paper, Scissors</h1>
           <Route exact path="/signup" component={SignupFormContainer} />
+          <Route exact path="/login" component={LoginContainer} />
         </div>
       </div>
     </Provider>
