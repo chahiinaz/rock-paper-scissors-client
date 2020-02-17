1fa9625adc36388c81d2eda0eceda04be38304a8
rickhekman <rickhekman@gmail.com>
2020-02-17 14:28:14 +0100

    add: Store

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// import { Provider } from 'react-redux'
+// import { Route } from 'react-router-dom'
+// import store from './store'
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="heading-primary">
+        <h1>Rock, Paper, Scissors</h1>
+      </div>
     </div>
+
   );
 }
 
+
 export default App;
+
+
+{/* <Provider store={store}>  </Provider> */ }
